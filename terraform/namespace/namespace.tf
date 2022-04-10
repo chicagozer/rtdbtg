2b308a762d95e01f02ce3734a696d0842467459e
Jim Mittler <jim.mittlerii@coxautoinc.com>
2022-04-10 17:14:23 +0000

    try setting up dns

--- terraform/namespace/namespace.tf
+++ terraform/namespace/namespace.tf
@@ -10,3 +10,41 @@ resource "kubernetes_namespace" "namespace" {
     name = var.namespace
   }
 }
+
+
+data "aws_lb" "ingress" {
+   tags = {
+     "elbv2.k8s.aws/cluster" = var.cluster
+   }
+ }
+
+
+data "aws_route53_zone" "zone" {
+  name         = "${var.domain}."
+  private_zone = false
+}
+
+
+resource "aws_route53_record" "namespace" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.namespace}.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+
+   alias {
+    name                   = data.aws_lb.ingress.dns_name
+    zone_id                = data.aws_lb.ingress.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "wildcard" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "*.${var.namespace}.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+
+   alias {
+    name                   = aws_route53_record.namespace.name
+    zone_id                = aws_route53_record.namespace.zone_id
+    evaluate_target_health = true
+  }
+}
--- terraform/namespace/variables.tf
+++ terraform/namespace/variables.tf
@@ -14,6 +14,14 @@ variable "tag" {
    default = "latest"
 }
 
+variable "cluster" {
+   default = "jem-test"
+}
+
+variable "domain" {
+   default = "coxeksdemo.com"
+}
+
 variable "app_version" {
   type = map
   description = "version to deploy"
